0f75eadbe861b02d64ce757b2d29180a2ff4fad9
Thierry Chappuis <tchappui@gmail.com>
2021-01-08 15:55:26 +0100

    Initial commit

---
+++ exemple.py
@@ -0,0 +1,17 @@
+import curses
+
+# start an application
+stdscr = curses.initscr() # Détermine le type de terminal, configure le terminal et crée diverses structures de données internes.
+curses.noecho() # Désactive l'écho automatique des touches à l'écran
+curses.cbreak() # Suppression du tampon
+stdscr.keypad(True) # Activation de la gestion des touches spéciales
+curses.start_color() # Active la gestion des couleurs
+
+stdscr.addstr(0, 0, "Ceci est un essai")
+stdscr.getkey()
+
+# stop an application
+stdscr.keypad(False)
+curses.nocbreak()
+curses.echo()
+curses.endwin()
