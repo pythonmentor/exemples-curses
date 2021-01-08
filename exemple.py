6c4b43c9f949cfee0b9628207691f1f2beeac172
Thierry Chappuis <tchappui@gmail.com>
2021-01-08 16:02:04 +0100

    Used curses.wrapper to simplify init

--- exemple.py
+++ exemple.py
@@ -1,17 +1,8 @@
 import curses
 
-# start an application
-stdscr = curses.initscr() # Détermine le type de terminal, configure le terminal et crée diverses structures de données internes.
-curses.noecho() # Désactive l'écho automatique des touches à l'écran
-curses.cbreak() # Suppression du tampon
-stdscr.keypad(True) # Activation de la gestion des touches spéciales
-curses.start_color() # Active la gestion des couleurs
+def main(stdscr):
+    stdscr.addstr(0, 0, "Une première chaine de caractère avec curses.")
+    stdscr.getkey()
 
-stdscr.addstr(0, 0, "Une première chaine de caractère avec curses.")
-stdscr.getkey()
-
-# stop an application
-stdscr.keypad(False)
-curses.nocbreak()
-curses.echo()
-curses.endwin()
+if __name__ == "__main__":
+    curses.wrapper(main)
